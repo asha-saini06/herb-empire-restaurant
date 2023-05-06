9551b8206f6bb675ac3c6d60ebf6d864b1261b3f
Asha Saini <59530575+asha-saini06@users.noreply.github.com>
2023-05-06 19:02:42 +0530

    Added About page to Herb Empire

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,68 @@
 import React from "react";
 import Layout from "../components/Layout/Layout";
+import { Box, Typography } from "@mui/material";
+import "../styles/AboutStyle.css";
 
 const About = () => {
   return (
     <Layout>
-      <h1>About page</h1>
+      <Box
+        sx={{
+          my: 10,
+          textAlign: "center",
+          p: 15,
+          backgroundColor: "#2e8b57ad",
+
+          "& h4": {
+            fontWeight: "bold",
+            my: 2,
+            fontSize: "2.2rem",
+          },
+          "& p": {
+            textAlign: "justify",
+          },
+          "@media (max-width:600px)": {
+            mt: 0,
+            p: 5,
+            "& h4": {
+              fontSize: "1.6rem",
+            },
+          },
+        }}
+      >
+        <Typography variant="h4" color={"#fff"} fontFamily={"Great Vibes"}>
+          Welcome To My Restaurant
+        </Typography>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident a
+          magnam alias unde dolorum eos placeat cupiditate illo reprehenderit
+          modi incidunt, iusto vitae tempora et. Voluptatem, numquam porro
+          veniam corrupti rem, harum reprehenderit laboriosam voluptatibus amet
+          doloribus magni neque veritatis officia libero beatae autem ullam
+          cupiditate. Sunt cum labore doloremque mollitia minus consequatur,
+          nostrum temporibus, repellendus quidem neque tempore? Dolorum laborum
+          modi quasi sunt cum officiis velit. Sunt veritatis tempora,
+          praesentium quia labore, modi dolores nisi dolorem consequuntur hic
+          aliquid quo similique porro nostrum ipsum molestiae cum accusamus
+          tenetur id? Eveniet at ratione optio fugiat dolorum vel suscipit eos
+          doloremque?
+        </p>
+        <br />
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Necessitatibus, incidunt libero cum ducimus, corporis omnis qui
+          recusandae similique laboriosam a aspernatur magni repellendus quis
+          molestiae dolorum, tenetur optio. Officia, temporibus? Modi molestiae
+          amet commodi dolorum deleniti itaque repellendus harum esse veritatis
+          tenetur illum consequatur repellat soluta sit accusamus omnis unde est
+          beatae facilis, sequi dolorem. Neque, provident saepe, corporis non
+          nesciunt corrupti laborum eaque placeat dolores quas assumenda atque
+          porro rerum et alias! Nihil rem tempora eaque perspiciatis? Quasi
+          nostrum sunt incidunt quod minus, eos possimus eveniet nesciunt
+          laboriosam sed at repellat ipsam quibusdam sint maiores vero alias
+          totam explicabo!
+        </p>
+      </Box>
     </Layout>
   );
 };
